6d56cc1d833d7e82fda6cfd00091c2938eebc695
iamSK77 <sk77.mobilefix@gmail.com>
2020-09-25 16:28:26 +0530

    beta error

--- src/components/LineGraph.jsx
+++ src/components/LineGraph.jsx
@@ -7,18 +7,44 @@ const Linegraph = () => {
     // https://disease.sh/v3/..
     // covid-19/historical/all?lastdays=120
 
+    const buildChartData = (data, casesType='cases') => {
+        const chartData = [];
+        let lastDataPoint; //diff b/w two dates
+
+        for(let date in data.cases) {
+            if (lastDataPoint) {
+                const newDataPoint = {
+                    x: date,
+                    y: data[casesType][date] - lastDataPoint
+                }
+                chartData.push(newDataPoint);
+            }
+            lastDataPoint = data[casesType][data];
+        };
+        return chartData;
+    };
+
     useEffect(() => {
         fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
         .then(res => res.json())
         .then(data => {
 
+            // byDefault it is cases if wanna change it add 2nd param caseType
+            const chartData = buildChartData(data);
+            setData(chartData);
         });
-    }, [])
+    }, []);
 
     return (
         <div>
             <Line 
-                data
+                data = {{
+                    datasets: [{
+                        backgroundColor: "",
+                        borderColor: "#CC1034",
+                        data: data
+                    }]
+                }}
                 options
             />
         </div>
